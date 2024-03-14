20890dfd833b03b4a879afe040957466a9792272
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2024-03-14 07:31:18 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const url = require('url');
+
+const ListNome = ["Mago","Javier Figueiredo","Breno K olho","Negão da Bl"]
+
+http.createServer(function(req,res){
+    
+
+    var query = url.parse(req.url,true).query;
+
+
+    const pages = []
+    pages["/"] = "Bem vindo Mago"
+    pages["/Add"] = ListNome.push(query)
+    pages["/getNomes"] = JSON.stringify(ListNome)
+    pages["semEndereco"] = "Ainda sem endereço"
+
+    res.end(pages[req.url])
+
+
+}).listen(8080)
+
+
+
+console.log("Api Funfou")
